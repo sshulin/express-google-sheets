9e041b09a45efe11546d5253a6bf003fa8e364d4
Alexander Christphorov <hrst.work@gmail.com>
2019-10-28 00:14:38 +0700

    Added two apis for object formatted values

--- server.js
+++ server.js
@@ -62,3 +62,63 @@ app.post('/table/update', (req, res) => {
 		res.send(data);
 	});
 })
+
+app.post('/mapped/get', (req, res) => {
+
+	gsheets.authorize({
+		client_id: req.headers['client_id'],
+		client_secret: req.headers['client_secret'],
+		token: JSON.parse(req.headers['token'])
+	}, (oAuth) => {
+		let scheme;
+		let result = [];
+		let firstRow = +req.body.range.split(':')[0].replace(/\D/g,'');
+
+		gsheets.getData(oAuth, req.body.spreadsheetId, req.body.sheetTab + '!' + req.body.range, (data) => {
+
+			function fillResult () {
+				data.values.forEach((row, index) => {
+					let col = {};
+					col._rowIndex = index + firstRow;
+					row.forEach((colValue, colIndex) => {
+						col[scheme[colIndex]] = colValue;
+					});
+
+					if(col._rowIndex != 1) result.push(col);
+				});
+				res.send(result);
+			}
+
+			if(firstRow != 1) {
+				gsheets.getData(oAuth, req.body.spreadsheetId, req.body.sheetTab + '!A1:Z1', (schemeData) => {
+					scheme = data.values[0];
+					fillResult();
+				});
+			} else {
+				scheme = data.values[0];
+				fillResult();
+			}
+		});
+	})
+})
+
+app.post('/mapped/update', (req, res) => {
+	gsheets.authorize({
+		client_id: req.headers['client_id'],
+		client_secret: req.headers['client_secret'],
+		token: JSON.parse(req.headers['token'])
+	}, (oAuth) => {
+		let scheme;
+		let result = [];
+
+		gsheets.getData(oAuth, req.body.spreadsheetId, req.body.sheetTab + '!A1:Z1', (schemeData) => {
+			scheme = schemeData.values[0];
+			scheme.forEach((value) => {
+				result.push(req.body.value[value])
+			});
+			gsheets.updateData(oAuth, req.body.spreadsheetId, req.body.sheetTab + '!A' + req.body.value._rowIndex + ':Z' + req.body.value._rowIndex, [result], (data) => {
+				res.send(data);
+			});
+		});
+	})
+})
